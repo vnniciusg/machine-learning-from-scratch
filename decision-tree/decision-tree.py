45b9b24321797a1953c9612c50af5b52ac1f5287
vnn <vinnicius109@gmail.com>
2024-05-03 14:50:09 -0300

    feat: Add decision tree implementation

---
+++ decision-tree/decision-tree.py
@@ -0,0 +1,21 @@
+import numpy as np
+from collections import Counter
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None) -> None:
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def if_leaf_node(self):
+        return self.value is not None
+    
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None) -> None:
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+    
